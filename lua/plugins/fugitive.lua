8c63f99dd134a4890d4bcc23af1967b334810bfd
raznan <raznan000@gmail.com>
2026-06-08 18:04:21 +0700

    Fix diffget diffput not pulling entire chunk

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -6,6 +6,9 @@ return {
 		tag = "v3.7",
 		config = function()
 			setup_keymap()
+
+			-- see https://github.com/neovim/neovim/issues/22696#issuecomment-3906586437
+			vim.opt.diffopt:remove("linematch:40")
 		end,
 	},
 }
