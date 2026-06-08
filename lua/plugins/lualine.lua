6c6d2728db161c057b4db106b99d18a9d020946a
raznan <raznan000@gmail.com>
2026-06-08 20:03:46 +0700

    Specify lualine extension for common plugin

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,7 +1,21 @@
 return {
 	{
 		"nvim-lualine/lualine.nvim",
-		opts = { theme = "gruvbox" },
+		opts = {
+			theme = "gruvbox",
+			tabline = {
+				lualine_a = { "tabs" },
+			},
+			extensions = {
+				"quickfix",
+				"fugitive",
+				"mason",
+				"oil",
+				"trouble",
+				"man",
+				"lazy",
+			},
+		},
 		dependencies = { "nvim-tree/nvim-web-devicons" },
 	},
 }
