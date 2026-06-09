15ab3fc95fbbedfa7b2060a38fceeba4f32cbd59
raznan <raznan000@gmail.com>
2026-06-09 23:41:23 +0700

    Specify default background for notify

--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -2,8 +2,15 @@ return {
 	{
 		"rcarriga/nvim-notify",
 		tag = "v3.15.0",
-		config = function()
+		config = function(_, opts)
 			local notify = require("notify")
+			notify.setup(
+				vim.tbl_extend(
+					"force",
+					opts,
+					{ background_colour = "#000000" }
+				)
+			)
 			vim.notify = notify
 		end,
 	},
