6d5503e776710465009dee6a90d305e2c5f48be9
raznan <raznan000@gmail.com>
2026-06-09 23:42:29 +0700

    Change pyright to basedpyright lsp

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,24 +1,24 @@
 return {
-        {
-                "mason-org/mason.nvim",
-                config = true,
-        },
-        {
-                "mason-org/mason-lspconfig.nvim",
-                opts = {
-                        ensure_installed = { 
-                                "lua_ls",
-                                "pyright",
-                                "vimls",
-                                "cmake",
-                                "clangd",
-                                "gopls",
-                        },
-                        automatic_enable = true,
-                },
-                dependencies = {
-                        "mason-org/mason.nvim",
-                        "neovim/nvim-lspconfig",
-                },
-        }
+	{
+		"mason-org/mason.nvim",
+		config = true,
+	},
+	{
+		"mason-org/mason-lspconfig.nvim",
+		opts = {
+			ensure_installed = {
+				"lua_ls",
+				"basedpyright",
+				"vimls",
+				"cmake",
+				"clangd",
+				"gopls",
+			},
+			automatic_enable = true,
+		},
+		dependencies = {
+			"mason-org/mason.nvim",
+			"neovim/nvim-lspconfig",
+		},
+	},
 }
