fc0ac4ceafc93047455ce93bb16acc57c1735f2e
raznan <raznan000@gmail.com>
2026-06-13 15:27:42 +0700

    Move telescope-dap plugin to its own file

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,45 +1,5 @@
 return {
 	{
-		"nvim-telescope/telescope-dap.nvim",
-		config = function()
-			require("telescope").load_extension("dap")
-			local teledap = require("telescope").extensions.dap
-			vim.keymap.set(
-				"n",
-				"<leader>fdc",
-				teledap.configurations,
-				{
-					noremap = true,
-					desc = "Browse DAP configurations",
-				}
-			)
-			vim.keymap.set(
-				"n",
-				"<leader>fdb",
-				teledap.list_breakpoints,
-				{
-					noremap = true,
-					desc = "Browse DAP breakpoints",
-				}
-			)
-			vim.keymap.set("n", "<leader>fdv", teledap.variables, {
-				noremap = true,
-				desc = "Browse DAP variables",
-			})
-		end,
-		keys = {
-			{
-				"<leader>fdf",
-				"<cmd>Telescope dap frames",
-				desc = "Browse DAP stack frames",
-			},
-		},
-		dependencies = {
-			"nvim-telescope/telescope.nvim",
-			"mfussenegger/nvim-dap",
-		},
-	},
-	{
 		"rcarriga/nvim-dap-ui",
 		tag = "v4.0.0",
 		config = function()
