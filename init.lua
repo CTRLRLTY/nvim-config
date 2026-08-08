bc1c597822d518d7330ccc0c145ac5d20be51019
raznan <raznan000@gmail.com>
2026-08-08 21:37:42 +0700

    Remove relativenumber autocmd since its dumb

--- init.lua
+++ init.lua
@@ -8,25 +8,10 @@ vim.go.relativenumber = true
 -- vim.go.wildmode = "longest,list,full"
 -- vim.go.wildmenu = true
 
-require('config.lazy')
-require('config.lsp')
-require('config.maps')
+require("config.lazy")
+require("config.lsp")
+require("config.maps")
 
 if init_debug then
-        require "osv".launch({ port = 8086, blocking = true })
+	require("osv").launch({ port = 8086, blocking = true })
 end
-
-vim.api.nvim_create_autocmd("ModeChanged", {
-        callback = function(ev)
-                if not vim.o.number then
-                        return
-                end
-
-                if vim.fn.mode() == 'v' then
-                        vim.o.relativenumber = true
-                else
-                        vim.o.relativenumber = false
-                end
-        end,
-        group = vim.api.nvim_create_augroup('NumberToggle', { clear = true }),
-})
